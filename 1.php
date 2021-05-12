03f565b84dc26065361a6711c50b4dd6a35141f2
Гость <@b302-02.sttec.net>
2021-05-12 13:59:05 +0300

    12.02.2021

---
+++ 1.php
@@ -0,0 +1,88 @@
+<html>
+<head>
+<meta charset="utf-8">
+ <meta charset="UTF-8">
+  <title>HTML5 видео в качестве заднего фона</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+<table  align="center" border="5px">
+                  
+                 
+                     <tr>
+                      <td colspan="7" align="center">Автомобиль</td>
+                     </tr>
+                     
+                     <tr>
+                      <td colspan="2" align="center"><strong><i>Марка автомобиля</i></strong></td>
+                      <td><strong><i>Номерной знак</i></strong></td>
+                      <td><strong><i>VIN Код</i></strong></td>
+                      <td><strong><i>Цвет</i></strong></td>
+                      <td><strong><i>Серия/номер СТС</i></strong></td>
+                      <td><strong><i>Серия/номер ПТС</i></strong></td>
+                     </tr>  
+                     
+                     <tr>
+                      <td><img src="https://image-server.autospot.ru/images/1/0/2/2/1/2/1239356/origin/1239356.webp" width="120"  height="70"/></td>
+                      <td><img src="https://parkers-images.bauersecure.com/gallery-image/pagefiles/267750/static-exterior/1752x1168/bmw-x7-uk-024.jpg" width="120"  height="70" /></td>
+                      <td><p>У344ЕМ67</p></td>
+                      <td><p>DL8SJ465563415514</p></td>
+					  <td><p>Тёмно-синий</p></td>
+                      <td><p>6544 427879</p></td>
+                      <td><p>51ВА 301363</p></td>
+                     </tr>
+                     
+                      <tr>
+                          <td><img src="https://i.ytimg.com/vi/IL2MshHaAPM/maxresdefault.jpg" width="120"  height="70"/></td>
+                          <td><img src="https://avatars.mds.yandex.net/get-autoru-vos/2150583/84167ef1b952d3da50a3785213f9021d/1200x900n" width="120"  height="70" /></td>
+                          <td><p>М715КК64</p></td>
+                          <td><p>ЕТ8ТG472473134428</p></td>
+						  <td><p>Серебристый тёмно-синий</p></td>
+                          <td><p>1412 271589</p></td>
+						  <td><p>67ХС 845108</p></td>
+                  
+                      </tr>
+                      
+                      <tr>
+                          <td><img src="https://i.artfile.ru/3000x2000_1519107_[www.ArtFile.ru].jpg" width="120"  height="70"/></td>
+                          <td><img src="https://im0-tub-ru.yandex.net/i?id=03739092372fce1188bdbc1de4b25598&n=13&exp=1" width="120"  height="70" /></td>
+                          <td><p>К862ЕС64</p></td>
+                          <td><p>КS9RН452389819889</p></td>
+						  <td><p>Оливково-зелёный</p></td>
+                          <td><p>2554 637777</p></td>
+						  <td><p>94ТУ 297460</p></td>
+                       
+                     </tr>
+					  <tr>
+                          <td><img src="https://a.d-cd.net/d6AAAgDpruA-1920.jpg" width="120"  height="70"/></td>
+                          <td><img src="https://a.d-cd.net/e8cd6b9s-1920.jpg" width="120"  height="70" /></td>
+                          <td><p>К862ЕС64</p></td>
+                          <td><p>КS9RН452389819889</p></td>
+                          <td><p>Тёмно-красный</p></td>
+                          <td><p>6379 821294</p></td>
+                          <td><p>13ТА 369460</p></td>
+                        
+                     </tr>
+					  <tr>
+                          <td><img src="https://im0-tub-ru.yandex.net/i?id=055642f4d67fd9991ecae11ca2820cb8&n=13&exp=1" width="120"  height="70"/></td>
+                          <td><img src="https://im0-tub-ru.yandex.net/i?id=26de1b0e2e2e2ee9cac9d27b1393dc32&n=13&exp=1" width="120"  height="70" /></td>
+                          <td><p>К862ЕС64</p></td>
+                          <td><p>КS9RН452389819889</p></td>
+                          <td><p>Серо-синий</p></td>
+                          <td><p>6710 202367</p></td>
+                          <td><p>27ОО 760552</p></td>
+                          
+                     </tr>
+ </table>
+  <div  align="center">
+ <form action="4.php" method="POST">
+ <input type="submit" value="добавить">
+ </form>
+  <form action="2.php" method="POST">
+ <input type="submit" value="выход">
+</form>
+</div>
+</body>
+</html>
